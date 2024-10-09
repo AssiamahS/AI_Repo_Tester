4d7137b2210363ba102689571efd97663d99dfb6
SylvesterAssiamah <122726691+AssiamahS@users.noreply.github.com>
2024-10-09 15:16:11 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SubmitForm from './components/SubmitForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI Repo Tester</h1>
+      <SubmitForm />
+    </div>
+  );
+}
+
+export default App;
